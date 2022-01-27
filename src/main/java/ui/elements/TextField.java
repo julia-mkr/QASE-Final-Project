d6270f9298082fd862fef1327ceb7405655df227
julia-mkr <julie.mokrous19@gmail.com>
2022-01-27 23:01:47 +0300

    code fixes

--- src/main/java/ui/elements/TextField.java
+++ src/main/java/ui/elements/TextField.java
@@ -20,8 +20,9 @@ public class TextField {
     private static final String TEXT_FIELD = "//*[text()='%s']//ancestor::*[contains(@id, 'Group')]//p";
 
     public void writeTextIntoTextField(String text) {
-        JavascriptExecutor js = ((JavascriptExecutor)driver);
-        log.info(String.format("Typing the text: '%s' into the '%s' text field using JS", text, label));
-        js.executeScript("arguments[0].value='" + text + "';", driver.findElement(By.xpath(String.format(TEXT_FIELD, label))));
+        Waiters.waitForElementBecomesVisible(driver, By.xpath(String.format(TEXT_FIELD, label)), 10);
+        driver.findElement(By.xpath(String.format(TEXT_FIELD, label))).click();
+        log.info(String.format("Typing the text: '%s' into the '%s' text field", text, label));
+        driver.findElement(By.xpath(String.format(TEXT_FIELD, label))).sendKeys(text);
     }
 }
