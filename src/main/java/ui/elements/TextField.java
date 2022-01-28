6ef1ee73c6c0de6b32654033ef46961e6c806e8c
julia-mkr <julie.mokrous19@gmail.com>
2022-01-28 12:23:04 +0300

    fixes

--- src/main/java/ui/elements/TextField.java
+++ src/main/java/ui/elements/TextField.java
@@ -22,13 +22,11 @@ public class TextField {
     private static final String TEXT_FIELD = "//*[text()='%s']//ancestor::*[contains(@id, 'Group')]//p";
 
     public void writeTextIntoTextField(String text) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         try {
             driver.findElement(By.xpath(String.format(TEXT_FIELD, label))).click();
             log.info(String.format("Typing the text: '%s' into the '%s' text field", text, label));
             driver.findElement(By.xpath(String.format(TEXT_FIELD, label))).sendKeys(text);
         } catch (UnhandledAlertException e) {
-            wait.until(ExpectedConditions.alertIsPresent());
             driver.switchTo().alert().dismiss();
             log.info(String.format("Typing the text: '%s' into the '%s' text field", text, label));
             driver.findElement(By.xpath(String.format(TEXT_FIELD, label))).sendKeys(text);
@@ -37,8 +35,15 @@ public class TextField {
 
     public void writeTextIntoTextFieldWithJs(String text) {
         JavascriptExecutor js  = (JavascriptExecutor)driver;
-        Waiters.waitForElementBecomesVisible(driver, By.xpath(String.format(TEXT_FIELD, label)), 5);
-        js.executeScript("arguments[0].click();", driver.findElement(By.xpath(String.format(TEXT_FIELD, label))));
-        driver.findElement(By.xpath(String.format(TEXT_FIELD, label))).sendKeys(text);
+        try {
+            Waiters.waitForElementBecomesVisible(driver, By.xpath(String.format(TEXT_FIELD, label)), 5);
+            js.executeScript("arguments[0].click();", driver.findElement(By.xpath(String.format(TEXT_FIELD, label))));
+            log.info(String.format("Typing the text: '%s' into the '%s' text field", text, label));
+            driver.findElement(By.xpath(String.format(TEXT_FIELD, label))).sendKeys(text);
+        } catch (UnhandledAlertException e) {
+            driver.switchTo().alert().dismiss();
+            log.info(String.format("Typing the text: '%s' into the '%s' text field", text, label));
+            driver.findElement(By.xpath(String.format(TEXT_FIELD, label))).sendKeys(text);
+        }
     }
 }
