0a13cd05410666c71b729b3858462d410d05fda5
julia-mkr <julie.mokrous19@gmail.com>
2022-01-28 20:54:45 +0300

    code fixes

--- src/test/java/testData/apiTestData/ApiTestPlanData.java
+++ src/test/java/testData/apiTestData/ApiTestPlanData.java
@@ -10,6 +10,6 @@ public interface ApiTestPlanData extends ApiConstants {
     CreateTestPlan API_TEST_PLAN_DATA_1 = CreateTestPlan.builder()
             .title("Test Plan 5")
             .description("This is the description for Test Plan 5. It includes new test cases.")
-            .cases(new ArrayList<>(Arrays.asList(38, 39, 40)))
+            .cases(new ArrayList<>(Arrays.asList(38, 40, 328)))
             .build();
 }
