f20cb3dafaa6a6917c3cdc03210b67b4632d3469
julia-mkr <julie.mokrous19@gmail.com>
2022-01-29 13:35:14 +0300

    Code fixes added

--- src/main/java/ui/elements/TextField.java
+++ src/main/java/ui/elements/TextField.java
@@ -18,17 +18,30 @@ public class TextField {
     private static final String TEXT_FIELD = "//*[text()='%s']//ancestor::*[contains(@id, 'Group')]//p";
 
     public void writeTextIntoTextField(String text) {
-        Waiters.waitForElementBecomesVisible(driver, By.xpath(String.format(TEXT_FIELD, label)), 5);
-        driver.findElement(By.xpath(String.format(TEXT_FIELD, label))).click();
-        log.info(String.format("Typing the text: '%s' into the '%s' text field", text, label));
-        driver.findElement(By.xpath(String.format(TEXT_FIELD, label))).sendKeys(text);
+        try {
+            driver.findElement(By.xpath(String.format(TEXT_FIELD, label))).click();
+            log.info(String.format("Typing the text: '%s' into the '%s' text field", text, label));
+            driver.findElement(By.xpath(String.format(TEXT_FIELD, label))).sendKeys(text);
+        } catch (UnhandledAlertException e) {
+            Waiters.waitForAlertBecomesPresent(driver, 10);
+            driver.switchTo().alert().dismiss();
+            log.info(String.format("Typing the text: '%s' into the '%s' text field", text, label));
+            driver.findElement(By.xpath(String.format(TEXT_FIELD, label))).sendKeys(text);
+        }
     }
 
     public void writeTextIntoTextFieldWithJs(String text) {
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        Waiters.waitForElementBecomesVisible(driver, By.xpath(String.format(TEXT_FIELD, label)), 5);
-        js.executeScript("arguments[0].click();", driver.findElement(By.xpath(String.format(TEXT_FIELD, label))));
-        log.info(String.format("Typing the text: '%s' into the '%s' text field", text, label));
-        driver.findElement(By.xpath(String.format(TEXT_FIELD, label))).sendKeys(text);
+        JavascriptExecutor js  = (JavascriptExecutor)driver;
+        try {
+            Waiters.waitForElementBecomesVisible(driver, By.xpath(String.format(TEXT_FIELD, label)), 5);
+            js.executeScript("arguments[0].click();", driver.findElement(By.xpath(String.format(TEXT_FIELD, label))));
+            log.info(String.format("Typing the text: '%s' into the '%s' text field", text, label));
+            driver.findElement(By.xpath(String.format(TEXT_FIELD, label))).sendKeys(text);
+        } catch (UnhandledAlertException e) {
+            Waiters.waitForAlertBecomesPresent(driver, 10);
+            driver.switchTo().alert().dismiss();
+            log.info(String.format("Typing the text: '%s' into the '%s' text field", text, label));
+            driver.findElement(By.xpath(String.format(TEXT_FIELD, label))).sendKeys(text);
+        }
     }
 }
